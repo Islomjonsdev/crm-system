b4597cabaae58164be1f6cc27472c83b430e7714
Islomjonsdev <islomjon@immigrateable.com>
2025-01-10 11:31:20 +0500

    fixed done

--- app/(root)/(home)/layout.jsx
+++ app/(root)/(home)/layout.jsx
@@ -5,7 +5,7 @@ export default function HomeLayout({ children }) {
   return (
     <div className="flex gap-[30px] pt-[20px] pr-[40px] pb-[32px] pl-[20px]">
       <Sidebar />
-      <div className="w-[100%]">
+      <div className="w-full ms-[230px]">
         <Header />
         <div className="pt-[50px]">
         {children}

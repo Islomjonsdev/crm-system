900a123ea7d3e3fab139ea0e3f1fd38b084a6edd
Islomjonsdev <islomjon@immigrateable.com>
2025-01-13 17:43:19 +0500

    changed

--- app/(root)/(home)/messenger/page.jsx
+++ app/(root)/(home)/messenger/page.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+
+import { instance } from '@/api'
+import { useState, useEffect } from 'react'
 
 const Messenger = () => {
+  const [ product, setProduct ] = useState([])
+  console.log(product, "datasdatasdatas");
+
+  const fetchData = async () => {
+    try {
+      const res = await instance.get("/work")
+      console.log(res?.data);
+      setProduct(res?.data)
+    }catch (err) {
+      console.log(err);
+    }
+  }
+ 
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
-    <div>Messenger</div>
+    <div>
+      <div>
+        {product?.map((item, id) => (
+          <div key={id}>{item?.title}</div>
+        ))}
+      </div>
+    </div>
   )
 }
 
